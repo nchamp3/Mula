bd668df2c387578348974dbd51314cb25c3a6893
krishnachandu <kchand23@uic.edu>
2019-10-11 22:08:37 -0500

    Updated IP address

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, jsonify, \
     url_for, flash
 
+import pymongo
+
+
 # from sqlalchemy import create_engine, asc, desc, \
 #     func, distinct
 # from sqlalchemy.orm import sessionmaker
@@ -18,6 +21,10 @@ app = Flask(__name__)
 
 
 # Connect to database and create database session
+myclient = pymongo.MongoClient("mongodb+srv://moolaAdmin:hackathon@moola-4by6t.mongodb.net/test?retryWrites=true&w=majority")
+
+mydb = myclient["userDatabase"]
+
 # engine = create_engine('sqlite:///flaskstarter.db')
 # Base.metadata.bind = engine
 
@@ -35,4 +42,4 @@ def showMain():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='127.0.0.1', port=8000)
